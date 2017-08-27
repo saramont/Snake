11e8d80f783f94c6ebf891a995a5eb5e64d15b99
saramont <31258173+saramont@users.noreply.github.com>
2017-08-27 10:25:53 +0200

    Delete conf.lua

--- LICENSE.md/conf.lua
+++
@@ -1,9 +0,0 @@
-function love.conf(t)
-  t.window.title = 'Snake'
-  t.window.icon = 'assets/graphics/icon.png'
-  t.window.width = 24 * 25
-  t.window.height = 24 * 15 + 32
-  t.window.borderless = false
-  
-  t.console = false
-end
